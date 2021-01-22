28d58c5ce120d007492176512a4e5ca6076e0dcf
Patrick Y. Wu <pywu@umich.edu>
2021-01-22 00:17:10 -0500

    updated binary_trainer.py with tester

--- marmot/scripts/binary_trainer.py
+++ marmot/scripts/binary_trainer.py
@@ -1,5 +1,5 @@
 from marmot.data.text_processor import text_processor
-import torch 
+import torch
 from torch.utils.data import Dataset, DataLoader, RandomSampler
 import torch.nn.functional as F
 from transformers import AdamW, get_linear_schedule_with_warmup, get_cosine_schedule_with_warmup, get_cosine_with_hard_restarts_schedule_with_warmup, get_constant_schedule_with_warmup
@@ -123,3 +123,32 @@ def binary_trainer(model, bert_model, train_dataset, validation_dataset, epochs,
         print("Validation Loss: {:.4f}".format(avg_val_loss))
 
     return val_accuracy, val_f1_weighted, val_f1_macro, val_f1, val_precision, val_recall, val_rocauc, avg_val_loss
+
+def tester(model, bert_model, test_dataset):
+    test_dataloader = DataLoader(test_dataset)
+    tp = text_processor(bert_model=bert_model, device=device)
+
+    id_list = []
+    proba_list = []
+    label = []
+
+    model.eval()
+
+    for step, test in enumerate(test_dataloader):
+        caption_ii, caption_tti, caption_am = tp.extract_bert_inputs(test['image_caption'])
+        text_ii, text_tti, text_am = tp.extract_bert_inputs(test['text'])
+
+        img = test['image'].to(device)
+        pic = test['pic'].to(device)
+
+        with torch.no_grad():
+            test_out, _ = model(img, pic, caption_ii, caption_tti, caption_am, text_ii, text_tti, text_am)
+            test_proba = F.softmax(test_out, dim=1)
+            predicted = torch.argmax(test_out, dim=1)
+
+        id_list.append(test['id'].item())
+        proba_list.append(test_proba[:,1].item())
+        label.append(predicted.item())
+
+    predictions = pd.DataFrame({'id': id_list, 'proba': proba_list, 'label': label})
+    return predictions
