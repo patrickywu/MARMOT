7c37f1e7fc8c74fac1ef7d983e8db890b8274320
Patrick Y. Wu <pywu@umich.edu>
2021-06-23 01:55:23 -0400

    updated with text only training pipelines

---
+++ marmot/modules/bert_pipeline.py
@@ -0,0 +1,75 @@
+import torch
+from torch import nn
+from transformers import BertModel, BertConfig
+
+class bert_text_only_pipeline(nn.Module):
+    def __init__(self, bert_model, bert_dim=768, dropout_p_final_clf=0.1, intermediate_layer_final_clf=768*4, num_classes=2):
+        super().__init__()
+        # Record the bert_model
+        self.bert_model = bert_model
+
+        # Encoder
+        self.bert_clf = BertModel.from_pretrained(self.bert_model, output_attentions=True)
+
+        # Classifier
+        self.final_clf = nn.Sequential(nn.Linear(bert_dim, intermediate_layer_final_clf),
+                                       nn.ReLU(),
+                                       nn.Dropout(p=dropout_p_final_clf),
+                                       nn.Linear(intermediate_layer_final_clf, num_classes))
+
+    def forward(self, text_ii, text_tti, text_am):
+        # BERT output
+        bert_output = self.bert_clf(input_ids=text_ii, attention_mask=text_am, token_type_ids=text_tti)
+
+        # Classifier
+        bert_output_avg = torch.sum(bert_output[0]*text_am.unsqueeze(-1), dim=1) / (torch.sum(text_am, dim=1).unsqueeze(-1))
+        out = self.final_clf(bert_output_avg)
+
+        return out, bert_output
+
+class bert_textcaption_pipeline(nn.Module):
+    def __init__(self, bert_model, bert_dim=768, dropout_p_final_clf=0.1, intermediate_layer_final_clf=768*4, num_classes=2):
+        super().__init__()
+        # Record the bert_model
+        self.bert_model = bert_model
+
+        self.bert_clf.embeddings.token_type_embeddings = triple_tti
+
+        # Encoder
+        self.bert_clf = BertModel.from_pretrained(self.bert_model, output_attentions=True)
+
+        # Classifier
+        self.final_clf = nn.Sequential(nn.Linear(bert_dim, intermediate_layer_final_clf),
+                                       nn.ReLU(),
+                                       nn.Dropout(p=dropout_p_final_clf),
+                                       nn.Linear(intermediate_layer_final_clf, num_classes))
+
+    def forward(self, pic, caption_ii, caption_tti, caption_am, text_ii, text_tti, text_am):
+        # Create position embeddings
+        pos_text = torch.arange(text_ii.shape[1], dtype=torch.long).to(img.device)
+        pos_text = pos_text.unsqueeze(0).expand(text_ii.shape[0], text_ii.shape[1])
+        pos_caption = torch.arange(caption_ii.shape[1], dtype=torch.long).to(img.device)
+        pos_caption = pos_caption.unsqueeze(0).expand(caption_ii.shape[0], caption_ii.shape[1])
+        pos = torch.cat((pos_text, pos_caption), dim=1)
+
+        # Create token type IDs to input
+        tti_caption = caption_tti + 1
+        tti = torch.cat((text_tti, tti_caption), dim=1)
+
+        # Create attention masks to input
+        am = torch.cat((text_am, caption_am*pic.unsqueeze(-1)), dim=1)
+
+        # Obtain inputs embeds
+        text_embeds = self.bert_clf.embeddings.word_embeddings(text_ii)
+        caption_embeds = self.bert_clf.embeddings.word_embeddings(caption_ii)
+
+        inputs_embeds = torch.cat((text_embeds, caption_embeds), dim=1)
+
+        # BERT output
+        bert_output = self.bert_clf(attention_mask=am, token_type_ids=tti, position_ids=pos, inputs_embeds=inputs_embeds)
+
+        # Classifier
+        bert_output_avg = torch.sum(bert_output[0]*am.unsqueeze(-1), dim=1) / (torch.sum(am, dim=1).unsqueeze(-1))
+        out = self.final_clf(bert_output_avg)
+
+        return out, bert_output
--- marmot/scripts/binary_trainer.py
+++ marmot/scripts/binary_trainer.py
@@ -185,3 +185,255 @@ def tester(model, test_dataset):
 
     predictions = pd.DataFrame({'id': id_list, 'proba': proba_list, 'label': label})
     return predictions
+
+def binary_trainer_textcaption(model, train_dataset, validation_dataset, epochs, learning_rate, batch_size,
+                               gradient_clipping=False, gc_value=1.0, proportion_warmup_steps=0.1, weight=None, device='cuda'):
+    '''
+    model: the MARMOT model input
+    train_dataset: the training data
+    validation_dataset: the validation data
+    epochs: number of epochs
+    learning_rate: the learning rate used for the AdamW optimizer
+    batch_size: the batch size
+    gradient_clipping: whether to activate gradient clipping or not (by default, clips at 1.0)
+    gc_value: gradient clipping value
+    proportion_warmup_steps: how many steps to warm up in the optimizer
+    weight: if any weight on the two classes
+    device: whether to run on CPU or GPU
+    '''
+
+    optimizer = AdamW(model.parameters(),
+                      lr = learning_rate,
+                      betas = (0.9,0.98),
+                      eps = 1e-8)
+
+    train_dataloader = DataLoader(train_dataset,
+                                  sampler = RandomSampler(train_dataset),
+                                  batch_size = batch_size)
+
+    validation_dataloader = DataLoader(validation_dataset)
+
+    total_steps = epochs*len(train_dataloader)
+
+    scheduler = get_cosine_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=proportion_warmup_steps*total_steps, num_training_steps=total_steps, num_cycles=0.5)
+
+    tp = text_processor(bert_model=model.bert_model, device=device)
+
+    # Training
+    for epoch in range(epochs):
+        model.train()
+        start_time = time.time()
+        train_loss = 0
+
+        for step, batch in enumerate(train_dataloader):
+            if step % 40 == 0 and step != 0:
+                print("Batch {} of {}. Elapsed: {:.4f} seconds".format(step, len(train_dataloader), time.time() - start_time))
+
+            model.zero_grad()
+
+            caption_ii, caption_tti, caption_am = tp.extract_bert_inputs(batch['image_caption'])
+            text_ii, text_tti, text_am = tp.extract_bert_inputs(batch['text'])
+
+            pic = batch['pic'].to(device)
+            label = batch['label'].to(device)
+
+            out, _ = model(pic=pic, caption_ii=caption_ii, caption_tti=caption_tti, caption_am=caption_am, text_ii=text_ii, text_tti=text_tti, text_am=text_am)
+            loss = F.cross_entropy(input=out.view(-1,2), target=label, weight=weight, reduction='mean')
+
+            train_loss += loss.item()
+
+            loss.backward()
+            if gradient_clipping:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), gc_value) # prevent exploding gradients issue
+            optimizer.step()
+            scheduler.step()
+            model.zero_grad()
+
+        avg_training_loss = train_loss / len(train_dataloader)
+        print('(Epoch {} / {}): loss: {:.4f}'.format(epoch+1, epochs, avg_training_loss))
+        print('Time for Epoch: {:.4f}'.format(time.time() - start_time))
+
+        # Validation
+        model.eval()
+
+        val_loss = 0
+
+        y_predicted = []
+        y_proba = []
+        y_true = []
+
+        for val in validation_dataloader:
+            caption_ii, caption_tti, caption_am = tp.extract_bert_inputs(val['image_caption'])
+            text_ii, text_tti, text_am = tp.extract_bert_inputs(val['text'])
+
+            pic = val['pic'].to(device)
+            val_label = val['label'].to(device)
+
+            with torch.no_grad():
+                val_out, _ = model(pic, caption_ii, caption_tti, caption_am, text_ii, text_tti, text_am)
+                val_out = val_out.view(-1,2)
+                val_proba = F.softmax(val_out, dim=1)
+                val_predicted = torch.argmax(val_out, dim=1)
+                val_loss_ind = F.cross_entropy(input=val_out, target=val_label, weight=weight, reduction='mean').item()
+            # Update y_predicted and y_true
+            y_predicted.append(val_predicted)
+            y_proba.append(val_proba[:,1])
+            y_true.append(val_label)
+
+            # Validation loss
+            val_loss += val_loss_ind
+
+        y_predicted = torch.cat(y_predicted)
+        y_proba = torch.cat(y_proba)
+        y_true = torch.cat(y_true)
+
+        # Metrics
+        val_accuracy = accuracy_score(y_true.to('cpu'), y_predicted.to('cpu'))
+        print("Validation Accuracy: {:.4f}".format(val_accuracy))
+
+        val_f1_weighted = f1_score(y_true.to('cpu'), y_predicted.to('cpu'), average='weighted')
+        print("Validation F1 Score (Weighted): {:.4f}".format(val_f1_weighted))
+
+        val_f1_macro = f1_score(y_true.to('cpu'), y_predicted.to('cpu'), average='macro')
+        print("Validation F1 Score (Macro): {:.4f}".format(val_f1_macro))
+
+        val_f1 = f1_score(y_true.to('cpu'), y_predicted.to('cpu'), average=None)
+        print("Validation F1 Scores (By Class): Not Hit: {:.4f}, Hit: {:.4f}".format(*val_f1))
+
+        val_precision = precision_score(y_true.to('cpu'), y_predicted.to('cpu'), average=None)
+        print("Precision (By Class): Not Hit: {:.4f}, Hit: {:.4f}".format(*val_precision))
+
+        val_recall = recall_score(y_true.to('cpu'), y_predicted.to('cpu'), average=None)
+        print("Recall (By Class): Not Hit: {:.4f}, Hit: {:.4f}".format(*val_recall))
+
+        val_rocauc = roc_auc_score(y_true.to('cpu'), y_proba.to('cpu'))
+        print("ROC AUC: {:.4f}".format(val_rocauc))
+
+        avg_val_loss = val_loss / len(validation_dataloader)
+        print("Validation Loss: {:.4f}".format(avg_val_loss))
+
+    return val_accuracy, val_f1_weighted, val_f1_macro, val_f1, val_precision, val_recall, val_rocauc, avg_val_loss
+
+def binary_trainer_text(model, train_dataset, validation_dataset, epochs, learning_rate, batch_size,
+                        gradient_clipping=False, gc_value=1.0, proportion_warmup_steps=0.1, weight=None, device='cuda'):
+    '''
+    model: the MARMOT model input
+    train_dataset: the training data
+    validation_dataset: the validation data
+    epochs: number of epochs
+    learning_rate: the learning rate used for the AdamW optimizer
+    batch_size: the batch size
+    gradient_clipping: whether to activate gradient clipping or not (by default, clips at 1.0)
+    gc_value: gradient clipping value
+    proportion_warmup_steps: how many steps to warm up in the optimizer
+    weight: if any weight on the two classes
+    device: whether to run on CPU or GPU
+    '''
+
+    optimizer = AdamW(model.parameters(),
+                      lr = learning_rate,
+                      betas = (0.9,0.98),
+                      eps = 1e-8)
+
+    train_dataloader = DataLoader(train_dataset,
+                                  sampler = RandomSampler(train_dataset),
+                                  batch_size = batch_size)
+
+    validation_dataloader = DataLoader(validation_dataset)
+
+    total_steps = epochs*len(train_dataloader)
+
+    scheduler = get_cosine_schedule_with_warmup(optimizer=optimizer, num_warmup_steps=proportion_warmup_steps*total_steps, num_training_steps=total_steps, num_cycles=0.5)
+
+    tp = text_processor(bert_model=model.bert_model, device=device)
+
+    # Training
+    for epoch in range(epochs):
+        model.train()
+        start_time = time.time()
+        train_loss = 0
+
+        for step, batch in enumerate(train_dataloader):
+            if step % 40 == 0 and step != 0:
+                print("Batch {} of {}. Elapsed: {:.4f} seconds".format(step, len(train_dataloader), time.time() - start_time))
+
+            model.zero_grad()
+
+            text_ii, text_tti, text_am = tp.extract_bert_inputs(batch['text'])
+
+            label = batch['label'].to(device)
+
+            out, _ = model(text_ii=text_ii, text_tti=text_tti, text_am=text_am)
+            loss = F.cross_entropy(input=out.view(-1,2), target=label, weight=weight, reduction='mean')
+
+            train_loss += loss.item()
+
+            loss.backward()
+            if gradient_clipping:
+                torch.nn.utils.clip_grad_norm_(model.parameters(), gc_value) # prevent exploding gradients issue
+            optimizer.step()
+            scheduler.step()
+            model.zero_grad()
+
+        avg_training_loss = train_loss / len(train_dataloader)
+        print('(Epoch {} / {}): loss: {:.4f}'.format(epoch+1, epochs, avg_training_loss))
+        print('Time for Epoch: {:.4f}'.format(time.time() - start_time))
+
+        # Validation
+        model.eval()
+
+        val_loss = 0
+
+        y_predicted = []
+        y_proba = []
+        y_true = []
+
+        for val in validation_dataloader:
+            text_ii, text_tti, text_am = tp.extract_bert_inputs(val['text'])
+
+            val_label = val['label'].to(device)
+
+            with torch.no_grad():
+                val_out, _ = model(text_ii, text_tti, text_am)
+                val_out = val_out.view(-1,2)
+                val_proba = F.softmax(val_out, dim=1)
+                val_predicted = torch.argmax(val_out, dim=1)
+                val_loss_ind = F.cross_entropy(input=val_out, target=val_label, weight=weight, reduction='mean').item()
+            # Update y_predicted and y_true
+            y_predicted.append(val_predicted)
+            y_proba.append(val_proba[:,1])
+            y_true.append(val_label)
+
+            # Validation loss
+            val_loss += val_loss_ind
+
+        y_predicted = torch.cat(y_predicted)
+        y_proba = torch.cat(y_proba)
+        y_true = torch.cat(y_true)
+
+        # Metrics
+        val_accuracy = accuracy_score(y_true.to('cpu'), y_predicted.to('cpu'))
+        print("Validation Accuracy: {:.4f}".format(val_accuracy))
+
+        val_f1_weighted = f1_score(y_true.to('cpu'), y_predicted.to('cpu'), average='weighted')
+        print("Validation F1 Score (Weighted): {:.4f}".format(val_f1_weighted))
+
+        val_f1_macro = f1_score(y_true.to('cpu'), y_predicted.to('cpu'), average='macro')
+        print("Validation F1 Score (Macro): {:.4f}".format(val_f1_macro))
+
+        val_f1 = f1_score(y_true.to('cpu'), y_predicted.to('cpu'), average=None)
+        print("Validation F1 Scores (By Class): Not Hit: {:.4f}, Hit: {:.4f}".format(*val_f1))
+
+        val_precision = precision_score(y_true.to('cpu'), y_predicted.to('cpu'), average=None)
+        print("Precision (By Class): Not Hit: {:.4f}, Hit: {:.4f}".format(*val_precision))
+
+        val_recall = recall_score(y_true.to('cpu'), y_predicted.to('cpu'), average=None)
+        print("Recall (By Class): Not Hit: {:.4f}, Hit: {:.4f}".format(*val_recall))
+
+        val_rocauc = roc_auc_score(y_true.to('cpu'), y_proba.to('cpu'))
+        print("ROC AUC: {:.4f}".format(val_rocauc))
+
+        avg_val_loss = val_loss / len(validation_dataloader)
+        print("Validation Loss: {:.4f}".format(avg_val_loss))
+
+    return val_accuracy, val_f1_weighted, val_f1_macro, val_f1, val_precision, val_recall, val_rocauc, avg_val_loss
